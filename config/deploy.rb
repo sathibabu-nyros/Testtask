7d33e7c8af4279c0e7c0e8ad75f77cddd144fb4e
sathibabu-nyros <sathibabu_nyros@yahoo.com>
2016-07-13 17:55:28 +0530

    intial commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,12 +21,12 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 set :keep_releases, 2
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-set :puma_state, "/home/ubuntu/Testtask/shared/tmp/pids/puma.state"
-set :puma_pid, "/home/ubuntu/Testtask/shared/tmp/pids/puma.pid"
-set :puma_bind, "unix:///home/ubuntu/Testtask/shared/tmp/sockets/puma.sock"    #accept array for multi-bind
-set :puma_conf, "/home/ubuntu/Testtask/shared/puma.rb"
-set :puma_access_log, "/home/ubuntu/Testtask/shared/log/puma_error.log"
-set :puma_error_log, "/home/ubuntu/Testtask/shared/log/puma_access.log"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
 set :puma_role, :app
 set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
 set :puma_threads, [0, 8]
