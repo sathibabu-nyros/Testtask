c46645fcf710720b6d40557d88668a18e1a297d4
sathibabu-nyros <sathibabu_nyros@yahoo.com>
2016-07-13 17:59:11 +0530

    intial commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -61,7 +61,7 @@ set :puma_preload_app, true
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/application.yml')
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
