9efe4090d4d63dd59589b2a7428632eef289d5db
sathibabu-nyros <sathibabu_nyros@yahoo.com>
2016-07-14 09:36:08 +0530

    update image upload for s3 bucket

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ApplicationRecord
   # s3 bucket storage configuration 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" },
     :storage => :s3,   
-    :s3_region => 'us-west-2a',
+    :s3_region => 'us-east-1',
+    :url =>':s3_domain_url',
     :s3_credentials => "#{Rails.root}/config/aws.yml",
     :path => "users/:id/:style.:extension"
 
@@ -15,10 +16,7 @@ class User < ApplicationRecord
   validates :email, :phone_no,  uniqueness: true   
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i  
   
-  # validates :phone_no,:presence => true,
-  #                :numericality => true,
-  #                :length => { :minimum => 10, :maximum => 15 }
-  validates :avatar, attachment_presence: true
-  has_attached_file :avatar, :styles => { :medium =>     "300x300#", :thumb => "200x200#" }
+  
+  validates :avatar, attachment_presence: true  
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
